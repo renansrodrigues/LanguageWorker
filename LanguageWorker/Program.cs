8a6c83455bff405c30586b795e7f839ae623c106
Renan Rodrigues <r.rodrigues@avivatec.com.br>
2022-04-25 21:43:13 -0300

    Add project files.

---
+++ LanguageWorker/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using System;
+
+namespace LanguageWorker
+{
+    class Program
+    {
+
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("7a87dfd3d7a04f24b6c15f94893acf58");
+        private static readonly Uri endpoint = new Uri("https://reconhecimentotextofiap.cognitiveservices.azure.com/");
+        
+        static void LanguageDetection(TextAnalyticsClient client)
+        {
+            Console.Write("Digite uma frase para reconhecimento de idioma:");
+            Console.ReadKey();
+            
+            string text;
+            text = Console.ReadLine();
+            DetectedLanguage detectedLanguage = client.DetectLanguage(text);            
+            Console.WriteLine("O texto digitado é do idioma:");
+            Console.WriteLine($"\t{detectedLanguage.Name}\n");
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            LanguageDetection(client);
+
+            Console.Write("Pressione uma tecla para fechar");
+            Console.ReadKey();
+        }
+    }
+}
